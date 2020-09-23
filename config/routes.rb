fde8389cb33ff482e982c67712a2947e78bf1fb6
EC2 Default User <kapibara.bba0924@gmail.com>
2020-09-23 07:45:58 +0000

    [Add]Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   root to: 'homes#top'
   get 'home/about' => 'homes#show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :edit, :update]
+  
 end
