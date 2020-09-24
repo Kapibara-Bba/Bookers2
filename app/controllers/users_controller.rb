430f1aa9e71c8ca1b2d785a6ec7126c2c614ff66
EC2 Default User <kapibara.bba0924@gmail.com>
2020-09-24 07:08:15 +0000

    [Add]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to users_path(@user.id)
+    redirect_to books_path(@user.id)
   end
   
   private
