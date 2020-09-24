d66e1a37ace9317e954f12f01da3c526255f3355
EC2 Default User <kapibara.bba0924@gmail.com>
2020-09-24 06:30:02 +0000

    Bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,11 +14,11 @@ class ApplicationController < ActionController::Base
   end
   
   def after_sign_in_path_for(resource)
-    users_path
+    users_path(resource)
   end
   
   def after_sign_up_path_for(resource)
-    users_path
+    users_path(resource)
   end
 end
 
