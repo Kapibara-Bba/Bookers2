1cf0f0a1da918517909a39e44840f8a93ec830d8
EC2 Default User <kapibara.bba0924@gmail.com>
2020-09-25 12:18:11 +0000

    [Add]Bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,16 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :email, :username, :password, :password_confirmation]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    added_attrs = [ :email, :name, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:sign_in, keys: added_attrs)
   end
-  
+
   def after_sign_in_path_for(resource)
     users_path(resource)
   end
-  
+
   def after_sign_up_path_for(resource)
     users_path(resource)
   end
