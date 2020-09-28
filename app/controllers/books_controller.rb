1cf4fc6a4a89dd199724ab8b52c79060744b98b1
EC2 Default User <kapibara.bba0924@gmail.com>
2020-09-28 08:20:31 +0000

    [Add]Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,9 @@ class BooksController < ApplicationController
 
   def show
     @user = current_user
+    @user_s = User.all
     @book = Book.new
-    @book = Book.all
+    @books = Book.all
   end
 
   def edit
@@ -19,13 +20,13 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @books = Book.all
     @book.save
-    redirect_to book_path(@book)
+    redirect_to book_path(@books)
   end
 
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path(@book)
+    redirect_to book_path(@books)
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#show'
   devise_for :users
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show, :edit, :update]
-  
-  resources :books
-  
+  resources :users, only: [:index, :show, :edit, :create, :update]
+
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
+
 end
