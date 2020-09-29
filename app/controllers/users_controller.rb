021c95bbc3dc19cc571ce7cc33dc91759e90a38a
EC2 Default User <kapibara.bba0924@gmail.com>
2020-09-29 10:01:43 +0000

    [Add]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,18 +26,21 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:user_update] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render 'edit'
+    end
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-end
 
+end
