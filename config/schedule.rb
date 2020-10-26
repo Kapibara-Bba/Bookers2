45f8daa6e4373f145b20dda5d694462018bbc664
EC2 Default User <kapibara.bba0924@gmail.com>
2020-10-26 07:35:57 +0000

    [Add]cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,7 +18,7 @@ set :environment, :development
 #   rake "some:great:rake:task"
 # end
 #
-every 1.minutes  do
+every 1.days, at: '9:00 am' do
   # Rails内のメソッド実行
   runner "DailyMailer.confirmation"
 end
